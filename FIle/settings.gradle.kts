c8df9db33dbcc11dc7b9dfcc994438c955d9069b
ludoutangshui <113495237+LiJianhong-qwq@users.noreply.github.com>
2024-06-14 21:27:32 +0800

    Add files via upload

---
+++ FIle/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CP3407 Project"
+include(":app")
